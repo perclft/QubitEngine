42876f1b1d8b384bfd10fb04da9eac71f3fc08c3
Sahil Samra <154658010+perclft@users.noreply.github.com>
2025-12-08 17:08:39 -0800

    Create ServiceImpl.hpp

---
+++ backend/src/ServiceImpl.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "quantum.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+
+class QubitEngineServiceImpl final : public qubit_engine::QuantumCompute::Service {
+public:
+    grpc::Status RunCircuit(grpc::ServerContext* context, 
+                          const qubit_engine::CircuitRequest* request,
+                          qubit_engine::StateResponse* response) override;
+};
